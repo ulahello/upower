5b6bae572f39a05e78f6c0c35018725f05c97a4d
Richard Hughes <richard@hughsie.com>
2008-08-05 17:26:35 +0100

    abstract out the client and client device parts of the devkit-power tool

--- tools/dkp-tool.c
+++ tools/dkp-tool.c
@@ -50,8 +50,9 @@
 #include "dkp-object.h"
 #include "dkp-history-obj.h"
 
-static DBusGConnection *bus = NULL;
-static DBusGProxy *power_proxy = NULL;
+#include "dkp-client.h"
+#include "dkp-client-device.h"
+
 static GMainLoop *loop;
 
 static gboolean opt_enumerate = FALSE;
@@ -59,19 +60,15 @@ static gboolean opt_monitor = FALSE;
 static gboolean opt_monitor_detail = FALSE;
 static gchar *opt_show_info = FALSE;
 
-static gboolean dkp_tool_do_monitor (void);
-static void dkp_tool_show_device_info (const gchar *object_path);
-
 /**
  * dkp_tool_device_added_cb:
  **/
 static void
-dkp_tool_device_added_cb (DBusGProxy *proxy, const gchar *object_path, gpointer user_data)
+dkp_tool_device_added_cb (DkpClient *client, const DkpClientDevice *device, gpointer user_data)
 {
-	g_print ("added:     %s\n", object_path);
+	g_print ("added:     %s\n", dkp_client_device_get_object_path (device));
 	if (opt_monitor_detail) {
-		dkp_tool_show_device_info (object_path);
-		g_print ("\n");
+		dkp_client_device_print (device);
 	}
 }
 
@@ -79,13 +76,12 @@ dkp_tool_device_added_cb (DBusGProxy *proxy, const gchar *object_path, gpointer 
  * dkp_tool_device_changed_cb:
  **/
 static void
-dkp_tool_device_changed_cb (DBusGProxy *proxy, const gchar *object_path, gpointer user_data)
+dkp_tool_device_changed_cb (DkpClient *client, const DkpClientDevice *device, gpointer user_data)
 {
-	g_print ("changed:     %s\n", object_path);
+	g_print ("changed:     %s\n", dkp_client_device_get_object_path (device));
 	if (opt_monitor_detail) {
 		/* TODO: would be nice to just show the diff */
-		dkp_tool_show_device_info (object_path);
-		g_print ("\n");
+		dkp_client_device_print (device);
 	}
 }
 
@@ -93,193 +89,26 @@ dkp_tool_device_changed_cb (DBusGProxy *proxy, const gchar *object_path, gpointe
  * dkp_tool_device_removed_cb:
  **/
 static void
-dkp_tool_device_removed_cb (DBusGProxy *proxy, const gchar *object_path, gpointer user_data)
+dkp_tool_device_removed_cb (DkpClient *client, const DkpClientDevice *device, gpointer user_data)
 {
-	g_print ("removed:   %s\n", object_path);
-}
-
-/**
- * dkp_tool_get_device_properties:
- **/
-static GHashTable *
-dkp_tool_get_device_properties (DBusGConnection *bus, const char *object_path)
-{
-	GError *error;
-	GHashTable *hash_table = NULL;
-	DBusGProxy *proxy;
-	const char *ifname = "org.freedesktop.DeviceKit.Power.Device";
-
-	proxy = dbus_g_proxy_new_for_name (bus, "org.freedesktop.DeviceKit.Power",
-						object_path, "org.freedesktop.DBus.Properties");
-	error = NULL;
-	if (!dbus_g_proxy_call (proxy, "GetAll", &error,
-				G_TYPE_STRING, ifname,
-				G_TYPE_INVALID,
-				dbus_g_type_get_map ("GHashTable", G_TYPE_STRING, G_TYPE_VALUE),
-				&hash_table,
-				G_TYPE_INVALID)) {
-		dkp_debug ("Couldn't call GetAll() to get properties for %s: %s", object_path, error->message);
-		g_error_free (error);
-		goto out;
-	}
-
-out:
-	g_object_unref (proxy);
-	return hash_table;
+	g_print ("removed:   %s\n", dkp_client_device_get_object_path (device));
 }
 
 /**
  * dkp_tool_do_monitor:
  **/
 static gboolean
-dkp_tool_do_monitor (void)
+dkp_tool_do_monitor (DkpClient *client)
 {
 	g_print ("Monitoring activity from the power daemon. Press Ctrl+C to cancel.\n");
 
-	dbus_g_proxy_connect_signal (power_proxy, "DeviceAdded",
-				     G_CALLBACK (dkp_tool_device_added_cb), NULL, NULL);
-	dbus_g_proxy_connect_signal (power_proxy, "DeviceRemoved",
-				     G_CALLBACK (dkp_tool_device_removed_cb), NULL, NULL);
-	dbus_g_proxy_connect_signal (power_proxy, "DeviceChanged",
-				     G_CALLBACK (dkp_tool_device_changed_cb), NULL, NULL);
+	g_signal_connect (client, "added", G_CALLBACK (dkp_tool_device_added_cb), NULL);
+	g_signal_connect (client, "removed", G_CALLBACK (dkp_tool_device_removed_cb), NULL);
+	g_signal_connect (client, "changed", G_CALLBACK (dkp_tool_device_changed_cb), NULL);
+
 	g_main_loop_run (loop);
 
 	return FALSE;
-}
-
-/**
- * dkp_tool_get_device_stats:
- **/
-static gboolean
-dkp_tool_get_device_stats (DBusGConnection *bus, const char *object_path, const gchar *type, guint timespec)
-{
-	GError *error = NULL;
-	DBusGProxy *proxy;
-	GType g_type_gvalue_array;
-	GPtrArray *gvalue_ptr_array = NULL;
-	GValueArray *gva;
-	GValue *gv;
-	guint i;
-	DkpHistoryObj *obj;
-	GPtrArray *array;
-	gboolean ret;
-
-	proxy = dbus_g_proxy_new_for_name (bus, "org.freedesktop.DeviceKit.Power",
-					   object_path, "org.freedesktop.DeviceKit.Power.Source");
-
-	g_type_gvalue_array = dbus_g_type_get_collection ("GPtrArray",
-					dbus_g_type_get_struct("GValueArray",
-						G_TYPE_UINT,
-						G_TYPE_DOUBLE,
-						G_TYPE_STRING,
-						G_TYPE_INVALID));
-
-	/* get compound data */
-	ret = dbus_g_proxy_call (proxy, "GetStatistics", &error,
-				G_TYPE_STRING, type,
-				G_TYPE_UINT, timespec,
-				G_TYPE_INVALID,
-				g_type_gvalue_array, &gvalue_ptr_array,
-				G_TYPE_INVALID);
-	if (!ret) {
-		dkp_debug ("GetStatistics(%s,%i) on %s failed: %s", type, timespec, object_path, error->message);
-		g_error_free (error);
-		goto out;
-	}
-
-	/* no data */
-	if (gvalue_ptr_array->len == 0)
-		goto out;
-
-	/* convert */
-	array = g_ptr_array_sized_new (gvalue_ptr_array->len);
-	for (i=0; i<gvalue_ptr_array->len; i++) {
-		gva = (GValueArray *) g_ptr_array_index (gvalue_ptr_array, i);
-		obj = dkp_history_obj_new ();
-		/* 0 */
-		gv = g_value_array_get_nth (gva, 0);
-		obj->time = g_value_get_uint (gv);
-		g_value_unset (gv);
-		/* 1 */
-		gv = g_value_array_get_nth (gva, 1);
-		obj->value = g_value_get_double (gv);
-		g_value_unset (gv);
-		/* 2 */
-		gv = g_value_array_get_nth (gva, 2);
-		obj->state = dkp_source_state_from_text (g_value_get_string (gv));
-		g_value_unset (gv);
-		g_ptr_array_add (array, obj);
-		g_value_array_free (gva);
-	}
-
-	/* print */
-	g_print ("  statistics (%s)\n", type);
-	for (i=0; i<array->len; i++) {
-		obj = (DkpHistoryObj *) g_ptr_array_index (array, i);
-		g_print ("    %lu seconds\t%.2lf (%s)\n", time (NULL) - obj->time, obj->value, dkp_source_state_to_text (obj->state));
-	}
-	g_ptr_array_free (array, TRUE);
-
-out:
-	if (gvalue_ptr_array != NULL)
-		g_ptr_array_free (gvalue_ptr_array, TRUE);
-	g_object_unref (proxy);
-	return TRUE;
-}
-
-/**
- * dkp_tool_show_device_info:
- **/
-static void
-dkp_tool_show_device_info (const gchar *object_path)
-{
-	GHashTable *hash;
-	DkpObject *obj;
-
-	/* get all the properties */
-	hash = dkp_tool_get_device_properties (bus, object_path);
-	if (hash == NULL) {
-		g_print ("Cannot get device properties for %s\n", object_path);
-		return;
-	}
-
-	/* create an object and copy properties */
-	obj = dkp_object_new ();
-	dkp_object_set_from_map (obj, hash);
-
-	/* print to screen */
-	dkp_object_print (obj);
-
-	/* if we can, get stats */
-	dkp_tool_get_device_stats (bus, object_path, "charge", 120);
-	dkp_tool_get_device_stats (bus, object_path, "rate", 120);
-
-	/* tidy up */
-	dkp_object_free (obj);
-	g_hash_table_unref (hash);
-}
-
-/**
- * dkp_tool_enumerate_devices:
- **/
-static GPtrArray *
-dkp_tool_enumerate_devices (DBusGProxy *proxy)
-{
-	gboolean ret;
-	GError *error = NULL;
-	GPtrArray *devices = NULL;
-	GType g_type_array;
-
-	g_type_array = dbus_g_type_get_collection ("GPtrArray", DBUS_TYPE_G_OBJECT_PATH);
-	ret = dbus_g_proxy_call (proxy, "EnumerateDevices", &error,
-				 G_TYPE_INVALID,
-				 g_type_array, &devices,
-				 G_TYPE_INVALID);
-	if (!ret) {
-		dkp_warning ("Couldn't enumerate devices: %s", error->message);
-		g_error_free (error);
-	}
-	return devices;
 }
 
 /**
@@ -290,18 +119,20 @@ main (int argc, char **argv)
 {
 	int ret = 1;
 	GOptionContext *context;
-	GError *error = NULL;
 	gboolean verbose = FALSE;
 	gboolean opt_dump = FALSE;
 	unsigned int n;
 
+	DkpClient *client;
+	DkpClientDevice *device;
+
 	const GOptionEntry entries[] = {
 		{ "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, _("Show extra debugging information"), NULL },
 		{ "enumerate", 'e', 0, G_OPTION_ARG_NONE, &opt_enumerate, _("Enumerate objects paths for devices"), NULL },
-		{ "dump", 0, 0, G_OPTION_ARG_NONE, &opt_dump, _("Dump all parameters for all objects"), NULL },
-		{ "monitor", 0, 0, G_OPTION_ARG_NONE, &opt_monitor, _("Monitor activity from the power daemon"), NULL },
+		{ "dump", 'd', 0, G_OPTION_ARG_NONE, &opt_dump, _("Dump all parameters for all objects"), NULL },
+		{ "monitor", 'm', 0, G_OPTION_ARG_NONE, &opt_monitor, _("Monitor activity from the power daemon"), NULL },
 		{ "monitor-detail", 0, 0, G_OPTION_ARG_NONE, &opt_monitor_detail, _("Monitor with detail"), NULL },
-		{ "show-info", 0, 0, G_OPTION_ARG_STRING, &opt_show_info, _("Show information about object path"), NULL },
+		{ "show-info", 'i', 0, G_OPTION_ARG_STRING, &opt_show_info, _("Show information about object path"), NULL },
 		{ NULL }
 	};
 
@@ -314,24 +145,12 @@ main (int argc, char **argv)
 	dkp_debug_init (verbose);
 
 	loop = g_main_loop_new (NULL, FALSE);
-
-	bus = dbus_g_bus_get (DBUS_BUS_SYSTEM, &error);
-	if (bus == NULL) {
-		dkp_warning ("Couldn't connect to system bus: %s", error->message);
-		g_error_free (error);
-		goto out;
-	}
-
-	power_proxy = dbus_g_proxy_new_for_name (bus, "org.freedesktop.DeviceKit.Power",
-						 "/", "org.freedesktop.DeviceKit.Power");
-	dbus_g_proxy_add_signal (power_proxy, "DeviceAdded", G_TYPE_STRING, G_TYPE_INVALID);
-	dbus_g_proxy_add_signal (power_proxy, "DeviceRemoved", G_TYPE_STRING, G_TYPE_INVALID);
-	dbus_g_proxy_add_signal (power_proxy, "DeviceChanged", G_TYPE_STRING, G_TYPE_INVALID);
+	client = dkp_client_new ();
 
 	if (opt_enumerate || opt_dump) {
 		GPtrArray *devices;
 		const gchar *object_path;
-		devices = dkp_tool_enumerate_devices (power_proxy);
+		devices = dkp_client_enumerate_devices (client);
 		if (devices == NULL)
 			goto out;
 		for (n=0; n < devices->len; n++) {
@@ -340,25 +159,26 @@ main (int argc, char **argv)
 				g_print ("%s\n", object_path);
 			else {
 				g_print ("Device: %s\n", object_path);
-				dkp_tool_show_device_info (object_path);
+				device = dkp_client_device_new ();
+				dkp_client_device_set_object_path (device, object_path);
+				dkp_client_device_print (device);
+				g_object_unref (device);
 			}
 		}
 		g_ptr_array_foreach (devices, (GFunc) g_free, NULL);
 		g_ptr_array_free (devices, TRUE);
 	} else if (opt_monitor || opt_monitor_detail) {
-		if (!dkp_tool_do_monitor ())
+		if (!dkp_tool_do_monitor (client))
 			goto out;
 	} else if (opt_show_info != NULL) {
-		dkp_tool_show_device_info (opt_show_info);
+		device = dkp_client_device_new ();
+		dkp_client_device_set_object_path (device, opt_show_info);
+		dkp_client_device_print (device);
+		g_object_unref (device);
 	}
 
 	ret = 0;
-
 out:
-	if (power_proxy != NULL)
-		g_object_unref (power_proxy);
-	if (bus != NULL)
-		dbus_g_connection_unref (bus);
-
+	g_object_unref (client);
 	return ret;
 }
