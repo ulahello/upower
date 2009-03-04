57e849f64b9700895a456ee86d0a419e24efe9e8
Richard Hughes <richard@hughsie.com>
2009-03-04 13:27:38 +0000

    require I_KNOW_THE_DEVICEKIT_POWER_API_IS_SUBJECT_TO_CHANGE

---
+++ devkit-power-gobject/devicekit-power.h
@@ -0,0 +1,45 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2008 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef I_KNOW_THE_DEVICEKIT_POWER_API_IS_SUBJECT_TO_CHANGE
+#error You have to define I_KNOW_THE_DEVICEKIT_POWER_API_IS_SUBJECT_TO_CHANGE
+#endif
+
+#ifndef __DEVICEKIT_POWER_H__
+#define __DEVICEKIT_POWER_H__
+
+#define __DEVICEKIT_POWER_H_INSIDE__
+
+#include <devkit-power-gobject/dkp-client.h>
+#include <devkit-power-gobject/dkp-device.h>
+#include <devkit-power-gobject/dkp-enum.h>
+#include <devkit-power-gobject/dkp-history-obj.h>
+#include <devkit-power-gobject/dkp-object.h>
+#include <devkit-power-gobject/dkp-qos.h>
+#include <devkit-power-gobject/dkp-qos-obj.h>
+#include <devkit-power-gobject/dkp-stats-obj.h>
+#include <devkit-power-gobject/dkp-wakeups.h>
+#include <devkit-power-gobject/dkp-wakeups-obj.h>
+
+#undef __DEVICEKIT_POWER_H_INSIDE__
+
+#endif /* __DEVICEKIT_POWER_H__ */
+
