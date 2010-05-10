3952c6d98a60a491c2f438ca2ed31dcf60c6cebc
Richard Hughes <richard@hughsie.com>
2010-05-10 22:40:17 +0100

    Ensure we sent ::notify signals when UpDaemon properties change

--- src/up-daemon.c
+++ src/up-daemon.c
@@ -698,6 +698,7 @@ up_daemon_set_lid_is_closed (UpDaemon *daemon, gboolean lid_is_closed)
 	UpDaemonPrivate *priv = daemon->priv;
 	egg_debug ("lid_is_closed = %s", priv->lid_is_closed ? "yes" : "no");
 	priv->lid_is_closed = lid_is_closed;
+	g_object_notify (G_OBJECT (daemon), "lid-is-closed");
 }
 
 /**
@@ -709,6 +710,7 @@ up_daemon_set_lid_is_present (UpDaemon *daemon, gboolean lid_is_present)
 	UpDaemonPrivate *priv = daemon->priv;
 	egg_debug ("lid_is_present = %s", priv->lid_is_present ? "yes" : "no");
 	priv->lid_is_present = lid_is_present;
+	g_object_notify (G_OBJECT (daemon), "lid-is-present");
 }
 
 /**
@@ -720,6 +722,7 @@ up_daemon_set_on_battery (UpDaemon *daemon, gboolean on_battery)
 	UpDaemonPrivate *priv = daemon->priv;
 	egg_debug ("on_battery = %s", priv->on_battery ? "yes" : "no");
 	priv->on_battery = on_battery;
+	g_object_notify (G_OBJECT (daemon), "on-battery");
 }
 
 /**
@@ -731,6 +734,7 @@ up_daemon_set_on_low_battery (UpDaemon *daemon, gboolean on_low_battery)
 	UpDaemonPrivate *priv = daemon->priv;
 	egg_debug ("on_low_battery = %s", priv->on_low_battery ? "yes" : "no");
 	priv->on_low_battery = on_low_battery;
+	g_object_notify (G_OBJECT (daemon), "on-low-battery");
 }
 
 /**
