41e678a8f45ba46368c5af0c73773a53645c6617
Landry Breuil <landry@rhaalovely.net>
2011-02-27 22:28:42 +0100

    openbsd: Add GObject stuff + wip on apm_thread/apm_event_cb
    Signed-off-by: Richard Hughes <richard@hughsie.com>

--- src/openbsd/up-backend.c
+++ src/openbsd/up-backend.c
@@ -1,8 +1,49 @@
+#include <errno.h> /* errno */
+#include <fcntl.h> /* open() */
+/* kevent() */
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+
+/* APM macros */
+#include <machine/apmvar.h>
 
 #include "up-backend.h"
 #include "up-daemon.h"
+#include "up-marshal.h"
+#include "up-device.h"
 
 #define UP_BACKEND_SUSPEND_COMMAND	"/usr/sbin/zzz"
+
+static void	up_backend_class_init	(UpBackendClass	*klass);
+static void	up_backend_init	(UpBackend		*backend);
+static void	up_backend_finalize	(GObject		*object);
+
+#define UP_BACKEND_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), UP_TYPE_BACKEND, UpBackendPrivate))
+
+struct UpBackendPrivate
+{
+	UpDaemon		*daemon;
+	UpDevice		*device;
+	UpDeviceList		*device_list; /* unused */
+	GObject			*native;
+	GThread			*apm_thread;
+	int			apm_fd;
+};
+
+enum {
+	SIGNAL_DEVICE_ADDED,
+	SIGNAL_DEVICE_REMOVED,
+	SIGNAL_LAST
+};
+
+static guint signals [SIGNAL_LAST] = { 0 };
+
+G_DEFINE_TYPE (UpBackend, up_backend, G_TYPE_OBJECT)
+
+/**
+ * functions called by upower daemon
+ **/
 
 /**
  * up_backend_coldplug:
@@ -17,7 +58,11 @@
 gboolean
 up_backend_coldplug (UpBackend *backend, UpDaemon *daemon)
 {
+	backend->priv->daemon = g_object_ref (daemon);
+	backend->priv->device_list = up_daemon_get_device_list (daemon);
+	return TRUE;
 }
+
 
 /**
  * up_backend_get_powersave_command:
@@ -78,6 +123,72 @@ up_backend_get_used_swap (UpBackend *backend)
 }
 
 /**
+ * OpenBSD specific code
+ **/
+
+/* callback updating the device */
+static gboolean
+up_backend_apm_powerchange_event_cb(gpointer object)
+{
+	UpBackend *backend;
+
+	g_return_if_fail (UP_IS_BACKEND (object));
+	backend = UP_BACKEND (object);
+	g_message("Got event, in callback");
+}
+
+/* thread doing kqueue() on apm device */
+static gpointer
+up_backend_apm_event_thread(gpointer object)
+{
+	int kq, nevents;
+	struct kevent ev;
+	struct timespec ts = {600, 0}, sts = {0, 0};
+
+	UpBackend *backend;
+
+	g_return_if_fail (UP_IS_BACKEND (object));
+	backend = UP_BACKEND (object);
+
+	g_message("setting up apm thread");
+
+	/* open /dev/apm */
+	if ((backend->priv->apm_fd = open("/dev/apm", O_RDONLY)) == -1) {
+		if (errno != ENXIO && errno != ENOENT)
+			g_error("cannot open device file");
+	}
+	kq = kqueue();
+	if (kq <= 0)
+		g_error("kqueue");
+	EV_SET(&ev, backend->priv->apm_fd, EVFILT_READ, EV_ADD | EV_ENABLE | EV_CLEAR,
+	    0, 0, NULL);
+	nevents = 1;
+	if (kevent(kq, &ev, nevents, NULL, 0, &sts) < 0)
+		g_error("kevent");
+
+	/* blocking wait on kqueue */
+	for (;;) {
+		int rv;
+
+		/* 10mn timeout */
+		sts = ts;
+		if ((rv = kevent(kq, NULL, 0, &ev, 1, &sts)) < 0)
+			break;
+		if (!rv)
+			continue;
+		if (ev.ident == backend->priv->apm_fd && APM_EVENT_TYPE(ev.data) == APM_POWER_CHANGE ) {
+			/* g_idle_add the callback */
+			g_idle_add((GSourceFunc) up_backend_apm_powerchange_event_cb, backend);
+		}
+	}
+	/* shouldnt be reached ? */
+}
+
+/**
+ * GObject class functions
+ **/
+
+/**
  * up_backend_new:
  *
  * Return value: a new %UpBackend object.
@@ -85,5 +196,102 @@ up_backend_get_used_swap (UpBackend *backend)
 UpBackend *
 up_backend_new (void)
 {
-	return NULL;
+	UpBackend *backend;
+	backend = g_object_new (UP_TYPE_BACKEND, NULL);
+	return UP_BACKEND (backend);
 }
+
+/**
+ * up_backend_class_init:
+ * @klass: The UpBackendClass
+ **/
+static void
+up_backend_class_init (UpBackendClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	object_class->finalize = up_backend_finalize;
+
+	signals [SIGNAL_DEVICE_ADDED] =
+		g_signal_new ("device-added",
+			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
+			      G_STRUCT_OFFSET (UpBackendClass, device_added),
+			      NULL, NULL, up_marshal_VOID__POINTER_POINTER,
+			      G_TYPE_NONE, 2, G_TYPE_POINTER, G_TYPE_POINTER);
+	signals [SIGNAL_DEVICE_REMOVED] =
+		g_signal_new ("device-removed",
+			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
+			      G_STRUCT_OFFSET (UpBackendClass, device_removed),
+			      NULL, NULL, up_marshal_VOID__POINTER_POINTER,
+			      G_TYPE_NONE, 2, G_TYPE_POINTER, G_TYPE_POINTER);
+
+	g_type_class_add_private (klass, sizeof (UpBackendPrivate));
+}
+
+/**
+ * up_backend_init:
+ **/
+static void
+up_backend_init (UpBackend *backend)
+{
+	GError *err = NULL;
+
+	backend->priv = UP_BACKEND_GET_PRIVATE (backend);
+	backend->priv->daemon = NULL;
+	backend->priv->device_list = NULL;
+	backend->priv->native = g_object_new (UP_TYPE_DEVICE, NULL);
+	backend->priv->device = up_device_new ();
+
+	g_thread_init (NULL);
+	/* creates thread */
+	if((backend->priv->apm_thread = g_thread_create((GThreadFunc)up_backend_apm_event_thread, backend, FALSE, &err) == NULL))
+	{
+		g_warning("Thread create failed: %s", err->message);
+		g_error_free (err);
+	}
+
+	/* setup dummy */
+	g_object_set (backend->priv->device,
+		      "native-path", "/hal/blows/goats",
+		      "vendor", "hughsie",
+		      "model", "BAT1",
+		      "serial", "0001",
+		      "type", UP_DEVICE_KIND_BATTERY,
+		      "online", FALSE,
+		      "power-supply", TRUE,
+		      "is-present", TRUE,
+		      "is-rechargeable", TRUE,
+		      "has-history", FALSE,
+		      "has-statistics", FALSE,
+		      "state", UP_DEVICE_STATE_DISCHARGING,
+		      "energy", 0.0f,
+		      "energy-empty", 0.0f,
+		      "energy-full", 10.0f,
+		      "energy-full-design", 10.0f,
+		      "energy-rate", 5.0f,
+		      "percentage", 50.0f,
+		      NULL);
+}
+
+/**
+ * up_backend_finalize:
+ **/
+static void
+up_backend_finalize (GObject *object)
+{
+	UpBackend *backend;
+
+	g_return_if_fail (UP_IS_BACKEND (object));
+
+	backend = UP_BACKEND (object);
+
+	if (backend->priv->daemon != NULL)
+		g_object_unref (backend->priv->daemon);
+	if (backend->priv->device_list != NULL)
+		g_object_unref (backend->priv->device_list);
+	g_object_unref (backend->priv->native);
+	g_object_unref (backend->priv->device);
+	/* XXX stop apm_thread ? */
+
+	G_OBJECT_CLASS (up_backend_parent_class)->finalize (object);
+}
+
