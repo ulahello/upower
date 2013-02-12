9843589d2d80e6dc2b3f51338e64bd1da1c53860
Richard Hughes <richard@hughsie.com>
2013-02-12 10:53:49 +0000

    Require applications to define UPOWER_ENABLE_DEPRECATED to use deprecated functionality
    This can easily be done by doing #define UPOWER_ENABLE_DEPRECATED before
    "#include <upower.h>" or adding -DUPOWER_ENABLE_DEPRECATED to the cflags line in
    Makefile.am
    
    See http://lists.freedesktop.org/archives/devkit-devel/2013-January/001339.html
    for more information on future plans and for rationale.

--- libupower-glib/up-client.h
+++ libupower-glib/up-client.h
@@ -60,8 +60,10 @@ typedef struct
 	void			(*device_removed)	(UpClient		*client,
 							 UpDevice		*device);
 	void			(*changed)		(UpClient		*client);
+	/* FIXME: remove when we next break API */
 	void			(*notify_sleep)		(UpClient		*client,
 							 UpSleepKind		 sleep_kind);
+	/* FIXME: remove when we next break API */
 	void			(*notify_resume)	(UpClient		*client,
 							 UpSleepKind		 sleep_kind);
 	/*< private >*/
@@ -87,6 +89,7 @@ gboolean	 up_client_get_properties_sync		(UpClient		*client,
 gboolean	 up_client_enumerate_devices_sync	(UpClient		*client,
 							 GCancellable		*cancellable,
 							 GError			**error);
+#ifdef UPOWER_ENABLE_DEPRECATED
 gboolean	 up_client_suspend_sync			(UpClient		*client,
 							 GCancellable		*cancellable,
 							 GError			**error);
@@ -97,16 +100,21 @@ gboolean	 up_client_about_to_sleep_sync		(UpClient		*client,
 gboolean	 up_client_hibernate_sync		(UpClient		*client,
 							 GCancellable		*cancellable,
 							 GError			**error);
+#endif
 
 /* accessors */
 GPtrArray	*up_client_get_devices			(UpClient		*client);
 const gchar	*up_client_get_daemon_version		(UpClient		*client);
+#ifdef UPOWER_ENABLE_DEPRECATED
 gboolean	 up_client_get_can_hibernate		(UpClient		*client);
+#endif
 gboolean	 up_client_get_lid_is_closed		(UpClient		*client);
 gboolean	 up_client_get_lid_is_present		(UpClient		*client);
 gboolean	 up_client_get_lid_force_sleep		(UpClient		*client);
 gboolean	 up_client_get_is_docked		(UpClient		*client);
+#ifdef UPOWER_ENABLE_DEPRECATED
 gboolean	 up_client_get_can_suspend		(UpClient		*client);
+#endif
 gboolean	 up_client_get_on_battery		(UpClient		*client);
 gboolean	 up_client_get_on_low_battery		(UpClient		*client);
 
