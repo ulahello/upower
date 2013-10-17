8460a10c5e5afb971e09b2393327525e95d7aeec
Bastien Nocera <hadess@hadess.net>
2013-10-17 14:41:55 +0200

    linux: Remove debug

--- src/linux/up-backend.c
+++ src/linux/up-backend.c
@@ -401,7 +401,6 @@ up_backend_get_critical_action (UpBackend *backend)
 							 G_DBUS_CALL_FLAGS_NONE,
 							 -1, NULL, NULL);
 			action_available = check_action_result (result);
-			g_message ("got result %s", g_variant_print (result, TRUE));
 			g_variant_unref (result);
 
 			if (!action_available)
