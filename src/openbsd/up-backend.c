ulahello/upower

commit bd488facf19d5304796ab4412ba089b00341ead2

device: Move daemon/native object into construct only properties
Rather than this weird situation where they are immediately passed in
through the "coldplug" method. Doing this is the first step to make
UpDevice into an initable object (removing the "coldplug" method).

src/openbsd/up-backend.c
@@ -125,24 +125,20 @@ up_apm_device_get_online (UpDevice *device, gboolean * online)
 gboolean
 up_backend_coldplug (UpBackend *backend, UpDaemon *daemon)
 {
-	UpApmNative *acnative = NULL;
-	UpApmNative *battnative = NULL;
 	backend->priv->daemon = g_object_ref (daemon);
 
 	if (backend->priv->is_laptop)
 	{
 		up_backend_update_lid_status(daemon);
-		acnative = up_apm_native_new("/ac");
-		if (!up_device_coldplug (backend->priv->ac, backend->priv->daemon, G_OBJECT(acnative)))
+		if (!up_device_coldplug (backend->priv->ac))
 			g_warning ("failed to coldplug ac");
 		else
-			g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, acnative, backend->priv->ac);
+			g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, backend->priv->ac);
 
-		battnative = up_apm_native_new("/batt");
-		if (!up_device_coldplug (backend->priv->battery, backend->priv->daemon, G_OBJECT(battnative)))
+		if (!up_device_coldplug (backend->priv->battery))
 			g_warning ("failed to coldplug battery");
 		else
-			g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, battnative, backend->priv->battery);
+			g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, backend->priv->battery);
 	}
 
 	return TRUE;
@@ -576,13 +572,13 @@ up_backend_class_init (UpBackendClass *klass)
 			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
 			      G_STRUCT_OFFSET (UpBackendClass, device_added),
 			      NULL, NULL, NULL,
-			      G_TYPE_NONE, 2, G_TYPE_POINTER, G_TYPE_POINTER);
+			      G_TYPE_NONE, 1, UP_TYPE_DEVICE);
 	signals [SIGNAL_DEVICE_REMOVED] =
 		g_signal_new ("device-removed",
 			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
 			      G_STRUCT_OFFSET (UpBackendClass, device_removed),
 			      NULL, NULL, NULL,
-			      G_TYPE_NONE, 2, G_TYPE_POINTER, G_TYPE_POINTER);
+			      G_TYPE_NONE, 1, UP_TYPE_DEVICE);
 }
 
 /**
@@ -600,8 +596,18 @@ up_backend_init (UpBackend *backend)
 	g_debug("is_laptop:%d",backend->priv->is_laptop);
 	if (backend->priv->is_laptop)
 	{
-		backend->priv->ac = UP_DEVICE(up_device_new());
-		backend->priv->battery = UP_DEVICE(up_device_new ());
+		UpApmNative *acnative = NULL;
+		UpApmNative *battnative = NULL;
+
+		acnative = up_apm_native_new("/ac");
+		battnative = up_apm_native_new("/batt");
+
+		backend->priv->ac = UP_DEVICE(up_device_new (backend->priv->daemon, G_OBJECT(acnative)));
+		backend->priv->battery = UP_DEVICE(up_device_new (backend->priv->daemon, G_OBJECT(battnative)));
+
+		g_object_unref (acnative);
+		g_object_unref (battnative);
+
 		device_class = UP_DEVICE_GET_CLASS (backend->priv->battery);
 		device_class->get_on_battery = up_apm_device_get_on_battery;
 		device_class->get_online = up_apm_device_get_online;

src/dummy/up-backend.c
@@ -91,7 +91,7 @@ up_backend_add_cb (UpBackend *backend)
 	}
 
 	/* emit */
-	g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, backend->priv->native, backend->priv->device);
+	g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, backend->priv->device);
 
 	/* setup poll */
 	timer_id = g_timeout_add_seconds (2, (GSourceFunc) up_backend_changed_time_cb, backend);
@@ -202,13 +202,13 @@ up_backend_class_init (UpBackendClass *klass)
 			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
 			      G_STRUCT_OFFSET (UpBackendClass, device_added),
 			      NULL, NULL, NULL,
-			      G_TYPE_NONE, 2, G_TYPE_POINTER, G_TYPE_POINTER);
+			      G_TYPE_NONE, 1, UP_TYPE_DEVICE);
 	signals [SIGNAL_DEVICE_REMOVED] =
 		g_signal_new ("device-removed",
 			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
 			      G_STRUCT_OFFSET (UpBackendClass, device_removed),
 			      NULL, NULL, NULL,
-			      G_TYPE_NONE, 2, G_TYPE_POINTER, G_TYPE_POINTER);
+			      G_TYPE_NONE, 1, UP_TYPE_DEVICE);
 }
 
 /**

src/freebsd/up-backend.c
@@ -185,8 +185,8 @@ up_backend_create_new_device (UpBackend *backend, UpAcpiNative *native)
 	UpDevice *device;
 	gboolean ret;
 
-	device = UP_DEVICE (up_device_supply_new ());
-	ret = up_device_coldplug (device, backend->priv->daemon, G_OBJECT (native));
+	device = UP_DEVICE (up_device_supply_new (backend->priv->daemon, G_OBJECT (native)));
+	ret = up_device_coldplug (device);
 	if (!ret)
 		g_object_unref (device);
 	else {
@@ -209,7 +209,7 @@ up_backend_create_new_device (UpBackend *backend, UpAcpiNative *native)
 			}
 		}
 
-		g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, native, device);
+		g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, device);
 	}
 
 	return ret;
@@ -395,13 +395,13 @@ up_backend_class_init (UpBackendClass *klass)
 			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
 			      G_STRUCT_OFFSET (UpBackendClass, device_added),
 			      NULL, NULL, NULL,
-			      G_TYPE_NONE, 2, G_TYPE_POINTER, G_TYPE_POINTER);
+			      G_TYPE_NONE, 1, UP_TYPE_DEVICE);
 	signals [SIGNAL_DEVICE_REMOVED] =
 		g_signal_new ("device-removed",
 			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
 			      G_STRUCT_OFFSET (UpBackendClass, device_removed),
 			      NULL, NULL, NULL,
-			      G_TYPE_NONE, 2, G_TYPE_POINTER, G_TYPE_POINTER);
+			      G_TYPE_NONE, 1, UP_TYPE_DEVICE);
 }
 
 /**

src/linux/up-backend.c
@@ -122,11 +122,11 @@ up_backend_device_new (UpBackend *backend, GUdevDevice *native)
 	if (g_strcmp0 (subsys, "power_supply") == 0) {
 
 		/* are we a valid power supply */
-		device = UP_DEVICE (up_device_supply_new ());
+		device = UP_DEVICE (up_device_supply_new (backend->priv->daemon, G_OBJECT (native)));
 		g_object_set (G_OBJECT(device),
 			      "ignore-system-percentage", GPOINTER_TO_INT (is_macbook (NULL)),
 			      NULL);
-		ret = up_device_coldplug (device, backend->priv->daemon, G_OBJECT (native));
+		ret = up_device_coldplug (device);
 		if (ret)
 			goto out;
 
@@ -136,8 +136,8 @@ up_backend_device_new (UpBackend *backend, GUdevDevice *native)
 	} else if (g_strcmp0 (subsys, "tty") == 0) {
 
 		/* see if this is a Watts Up Pro device */
-		device = UP_DEVICE (up_device_wup_new ());
-		ret = up_device_coldplug (device, backend->priv->daemon, G_OBJECT (native));
+		device = UP_DEVICE (up_device_wup_new (backend->priv->daemon, G_OBJECT (native)));
+		ret = up_device_coldplug (device);
 		if (ret)
 			goto out;
 
@@ -148,16 +148,16 @@ up_backend_device_new (UpBackend *backend, GUdevDevice *native)
 
 #ifdef HAVE_IDEVICE
 		/* see if this is an iDevice */
-		device = UP_DEVICE (up_device_idevice_new ());
-		ret = up_device_coldplug (device, backend->priv->daemon, G_OBJECT (native));
+		device = UP_DEVICE (up_device_idevice_new (backend->priv->daemon, G_OBJECT (native)));
+		ret = up_device_coldplug (device);
 		if (ret)
 			goto out;
 		g_object_unref (device);
 #endif /* HAVE_IDEVICE */
 
 		/* try to detect a HID UPS */
-		device = UP_DEVICE (up_device_hid_new ());
-		ret = up_device_coldplug (device, backend->priv->daemon, G_OBJECT (native));
+		device = UP_DEVICE (up_device_hid_new (backend->priv->daemon, G_OBJECT (native)));
+		ret = up_device_coldplug (device);
 		if (ret)
 			goto out;
 
@@ -247,7 +247,7 @@ up_backend_device_add (UpBackend *backend, GUdevDevice *native, const char *was_
 		g_warning ("treated %s event as add on %s", was_event, g_udev_device_get_sysfs_path (native));
 
 	/* emit */
-	g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, native, device);
+	g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, device);
 out:
 	g_clear_object (&object);
 	return ret;
@@ -269,7 +269,7 @@ up_backend_device_remove (UpBackend *backend, GUdevDevice *native)
 	device = UP_DEVICE (object);
 	/* emit */
 	g_debug ("emitting device-removed: %s", g_udev_device_get_sysfs_path (native));
-	g_signal_emit (backend, signals[SIGNAL_DEVICE_REMOVED], 0, native, device);
+	g_signal_emit (backend, signals[SIGNAL_DEVICE_REMOVED], 0, device);
 
 out:
 	g_clear_object (&object);
@@ -358,7 +358,7 @@ bluez_interface_removed (GDBusObjectManager *manager,
 		return;
 
 	g_debug ("emitting device-removed: %s", g_dbus_object_get_object_path (bus_object));
-	g_signal_emit (backend, signals[SIGNAL_DEVICE_REMOVED], 0, bus_object, UP_DEVICE (object));
+	g_signal_emit (backend, signals[SIGNAL_DEVICE_REMOVED], 0, UP_DEVICE (object));
 
 	g_object_unref (object);
 }
@@ -383,15 +383,15 @@ bluez_interface_added (GDBusObjectManager *manager,
 		return;
 	}
 
-	device = UP_DEVICE (up_device_bluez_new ());
-	ret = up_device_coldplug (device, backend->priv->daemon, G_OBJECT (bus_object));
+	device = UP_DEVICE (up_device_bluez_new (backend->priv->daemon, G_OBJECT (bus_object)));
+	ret = up_device_coldplug (device);
 	if (!ret) {
 		g_object_unref (device);
 		return;
 	}
 
 	g_debug ("emitting device-added: %s", g_dbus_object_get_object_path (bus_object));
-	g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, bus_object, device);
+	g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, device);
 }
 
 static void
@@ -470,7 +470,7 @@ bluez_vanished (GDBusConnection *connection,
 
 			object = G_DBUS_OBJECT (up_device_get_native (device));
 			g_debug ("emitting device-removed: %s", g_dbus_object_get_object_path (object));
-			g_signal_emit (backend, signals[SIGNAL_DEVICE_REMOVED], 0, object, UP_DEVICE (object));
+			g_signal_emit (backend, signals[SIGNAL_DEVICE_REMOVED], 0, UP_DEVICE (object));
 		}
 	}
 
@@ -785,13 +785,13 @@ up_backend_class_init (UpBackendClass *klass)
 			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
 			      G_STRUCT_OFFSET (UpBackendClass, device_added),
 			      NULL, NULL, NULL,
-			      G_TYPE_NONE, 2, G_TYPE_POINTER, G_TYPE_POINTER);
+			      G_TYPE_NONE, 1, UP_TYPE_DEVICE);
 	signals [SIGNAL_DEVICE_REMOVED] =
 		g_signal_new ("device-removed",
 			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
 			      G_STRUCT_OFFSET (UpBackendClass, device_removed),
 			      NULL, NULL, NULL,
-			      G_TYPE_NONE, 2, G_TYPE_POINTER, G_TYPE_POINTER);
+			      G_TYPE_NONE, 1, UP_TYPE_DEVICE);
 }
 
 static void

src/freebsd/up-device-supply.c
@@ -485,8 +485,11 @@ up_device_supply_class_init (UpDeviceSupplyClass *klass)
  * up_device_supply_new:
  **/
 UpDeviceSupply *
-up_device_supply_new (void)
+up_device_supply_new (UpDaemon	*daemon,
+                      GObject	*native)
 {
-	return g_object_new (UP_TYPE_DEVICE_SUPPLY, NULL);
+	return UP_DEVICE_SUPPLY (g_object_new (UP_TYPE_DEVICE_SUPPLY,
+	                                       "daemon", daemon,
+	                                       "native", native,
+	                                       NULL));
 }
-

src/freebsd/up-device-supply.h
@@ -48,7 +48,8 @@ typedef struct
 } UpDeviceSupplyClass;
 
 GType		 up_device_supply_get_type		(void);
-UpDeviceSupply	*up_device_supply_new			(void);
+UpDeviceSupply	*up_device_supply_new			(UpDaemon	*daemon,
+							 GObject	*native);
 
 G_END_DECLS
 

src/linux/up-device-bluez.c
@@ -291,8 +291,11 @@ up_device_bluez_class_init (UpDeviceBluezClass *klass)
 }
 
 UpDeviceBluez *
-up_device_bluez_new (void)
+up_device_bluez_new (UpDaemon	*daemon,
+                     GObject	*native)
 {
-	return g_object_new (UP_TYPE_DEVICE_BLUEZ, NULL);
+	return UP_DEVICE_BLUEZ (g_object_new (UP_TYPE_DEVICE_BLUEZ,
+	                                      "daemon", daemon,
+	                                      "native", native,
+	                                      NULL));
 }
-

src/linux/up-device-bluez.h
@@ -46,7 +46,8 @@ typedef struct
 } UpDeviceBluezClass;
 
 GType		 up_device_bluez_get_type		(void);
-UpDeviceBluez	*up_device_bluez_new			(void);
+UpDeviceBluez	*up_device_bluez_new			(UpDaemon	*daemon,
+							 GObject	*native);
 void		 up_device_bluez_update			(UpDeviceBluez *bluez,
 							 GVariant      *properties);
 

commit b643f272a70edb5ee4107c861a77973efdc94213

openbsd: move most initialization code from _init to _coldplug (fixes #201)
regression from the refactoring in bd488fac

src/openbsd/up-backend.c
@@ -33,6 +33,7 @@ static void	up_backend_init	(UpBackend		*backend);
 static void	up_backend_finalize	(GObject		*object);
 
 static gboolean	up_backend_apm_get_power_info(struct apm_power_info*);
+static gpointer	up_backend_apm_event_thread(gpointer object);
 UpDeviceState up_backend_apm_get_battery_state_value(u_char battery_state);
 static void	up_backend_update_acpibat_state(UpDevice*, struct sensordev);
 static void	up_backend_update_lid_status(UpDaemon*);
@@ -127,10 +128,63 @@ up_apm_device_get_online (UpDevice *device, gboolean * online)
 gboolean
 up_backend_coldplug (UpBackend *backend, UpDaemon *daemon)
 {
-	backend->priv->daemon = g_object_ref (daemon);
+	GError *err = NULL;
+	UpDeviceClass *device_class;
+	gint64 current_time;
 
+	backend->priv = up_backend_get_instance_private (backend);
+	backend->priv->daemon = g_object_ref (daemon);
+	backend->priv->is_laptop = up_native_is_laptop();
+	g_debug("is_laptop:%d",backend->priv->is_laptop);
 	if (backend->priv->is_laptop)
 	{
+		UpApmNative *acnative = NULL;
+		UpApmNative *battnative = NULL;
+
+		acnative = up_apm_native_new("/ac");
+		battnative = up_apm_native_new("/batt");
+
+		backend->priv->ac = UP_DEVICE(up_device_new (backend->priv->daemon, G_OBJECT(acnative)));
+		backend->priv->battery = UP_DEVICE(up_device_new (backend->priv->daemon, G_OBJECT(battnative)));
+
+		g_object_unref (acnative);
+		g_object_unref (battnative);
+
+		device_class = UP_DEVICE_GET_CLASS (backend->priv->battery);
+		device_class->get_on_battery = up_apm_device_get_on_battery;
+		device_class->get_online = up_apm_device_get_online;
+		device_class->refresh = up_apm_device_refresh;
+		device_class = UP_DEVICE_GET_CLASS (backend->priv->ac);
+		device_class->get_on_battery = up_apm_device_get_on_battery;
+		device_class->get_online = up_apm_device_get_online;
+		device_class->refresh = up_apm_device_refresh;
+		/* creates thread */
+		if((backend->priv->apm_thread = (GThread*) g_thread_try_new("apm-poller",(GThreadFunc)up_backend_apm_event_thread, (void*) backend, &err)) == NULL)
+		{
+			g_warning("Thread create failed: %s", err->message);
+			g_error_free (err);
+		}
+
+		/* setup dummy */
+		current_time = g_get_real_time () / G_USEC_PER_SEC;
+		g_object_set (backend->priv->battery,
+			      "type", UP_DEVICE_KIND_BATTERY,
+			      "power-supply", TRUE,
+			      "is-present", TRUE,
+			      "is-rechargeable", TRUE,
+			      "has-history", TRUE,
+			      "state", UP_DEVICE_STATE_UNKNOWN,
+			      "percentage", 0.0f,
+			      "time-to-empty", (gint64) 0,
+			      "update-time", (guint64) current_time,
+			      (void*) NULL);
+		g_object_set (backend->priv->ac,
+			      "type", UP_DEVICE_KIND_LINE_POWER,
+			      "online", TRUE,
+			      "power-supply", TRUE,
+			      "update-time", (guint64) current_time,
+			      (void*) NULL);
+
 		up_backend_update_lid_status(daemon);
 		if (!g_initable_init (G_INITABLE (backend->priv->ac), NULL, NULL))
 			g_warning ("failed to coldplug ac");
@@ -142,7 +196,6 @@ up_backend_coldplug (UpBackend *backend, UpDaemon *daemon)
 		else
 			g_signal_emit (backend, signals[SIGNAL_DEVICE_ADDED], 0, backend->priv->battery);
 	}
-
 	return TRUE;
 }
 
@@ -597,63 +650,7 @@ up_backend_class_init (UpBackendClass *klass)
 static void
 up_backend_init (UpBackend *backend)
 {
-	GError *err = NULL;
-	UpDeviceClass *device_class;
-	gint64 current_time;
-
 	backend->priv = up_backend_get_instance_private (backend);
-	backend->priv->is_laptop = up_native_is_laptop();
-	g_debug("is_laptop:%d",backend->priv->is_laptop);
-	if (backend->priv->is_laptop)
-	{
-		UpApmNative *acnative = NULL;
-		UpApmNative *battnative = NULL;
-
-		acnative = up_apm_native_new("/ac");
-		battnative = up_apm_native_new("/batt");
-
-		backend->priv->ac = UP_DEVICE(up_device_new (backend->priv->daemon, G_OBJECT(acnative)));
-		backend->priv->battery = UP_DEVICE(up_device_new (backend->priv->daemon, G_OBJECT(battnative)));
-
-		g_object_unref (acnative);
-		g_object_unref (battnative);
-
-		device_class = UP_DEVICE_GET_CLASS (backend->priv->battery);
-		device_class->get_on_battery = up_apm_device_get_on_battery;
-		device_class->get_online = up_apm_device_get_online;
-		device_class->refresh = up_apm_device_refresh;
-		device_class = UP_DEVICE_GET_CLASS (backend->priv->ac);
-		device_class->get_on_battery = up_apm_device_get_on_battery;
-		device_class->get_online = up_apm_device_get_online;
-		device_class->refresh = up_apm_device_refresh;
-		/* creates thread */
-		if((backend->priv->apm_thread = (GThread*) g_thread_try_new("apm-poller",(GThreadFunc)up_backend_apm_event_thread, (void*) backend, &err)) == NULL)
-		{
-			g_warning("Thread create failed: %s", err->message);
-			g_error_free (err);
-		}
-
-		/* setup dummy */
-		current_time = g_get_real_time () / G_USEC_PER_SEC;
-		g_object_set (backend->priv->battery,
-			      "type", UP_DEVICE_KIND_BATTERY,
-			      "power-supply", TRUE,
-			      "is-present", TRUE,
-			      "is-rechargeable", TRUE,
-			      "has-history", TRUE,
-			      "state", UP_DEVICE_STATE_UNKNOWN,
-			      "percentage", 0.0f,
-			      "time-to-empty", (gint64) 0,
-			      "update-time", (guint64) current_time,
-			      (void*) NULL);
-		g_object_set (backend->priv->ac,
-			      "type", UP_DEVICE_KIND_LINE_POWER,
-			      "online", TRUE,
-			      "power-supply", TRUE,
-			      "update-time", (guint64) current_time,
-			      (void*) NULL);
-	}
-
 	backend->priv->config = up_config_new ();
 	backend->priv->seat_manager_proxy = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SYSTEM,
 									   0,