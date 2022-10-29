ulahello/upower

commit 1550d50f20895f5918d99ed61958b144c3bd4327

linux: Remove "usb" subsystem match
The hiddev driver has been in the "usbmisc" subsystem since kernel 3.7.0
or so. As such, we can simply drop the "usb" subsystem from our matches
at this point.

src/linux/integration-test.py
@@ -842,7 +842,7 @@ class Tests(dbusmock.DBusTestCase):
         '''UPS properties without AC'''
 
         # add a charging UPS
-        ups0 = self.testbed.add_device('usb', 'hiddev0', None, [],
+        ups0 = self.testbed.add_device('usbmisc', 'hiddev0', None, [],
                                        ['DEVNAME', 'null', 'UPOWER_VENDOR', 'APC',
                                         'UPOWER_BATTERY_TYPE', 'ups',
                                         'UPOWER_FAKE_DEVICE', '1',
@@ -893,7 +893,7 @@ class Tests(dbusmock.DBusTestCase):
         '''UPS properties with offline AC'''
 
         # add low charge UPS
-        ups0 = self.testbed.add_device('usb', 'hiddev0', None, [],
+        ups0 = self.testbed.add_device('usbmisc', 'hiddev0', None, [],
                                        ['DEVNAME', 'null', 'UPOWER_VENDOR', 'APC',
                                         'UPOWER_BATTERY_TYPE', 'ups',
                                         'UPOWER_FAKE_DEVICE', '1',

src/linux/up-backend.c
@@ -144,7 +144,7 @@ up_backend_device_new (UpBackend *backend, GUdevDevice *native)
 		/* no valid TTY object */
 		g_clear_object (&device);
 
-	} else if (g_strcmp0 (subsys, "usb") == 0 || g_strcmp0 (subsys, "usbmisc") == 0) {
+	} else if (g_strcmp0 (subsys, "usbmisc") == 0) {
 
 #ifdef HAVE_IDEVICE
 		/* see if this is an iDevice */
@@ -496,8 +496,8 @@ up_backend_coldplug (UpBackend *backend, UpDaemon *daemon)
 	GList *devices;
 	GList *l;
 	guint i;
-	const gchar *subsystems_wup[] = {"power_supply", "usb", "usbmisc", "tty", "input", NULL};
-	const gchar *subsystems[] = {"power_supply", "usb", "usbmisc", "input", NULL};
+	const gchar *subsystems_wup[] = {"power_supply", "usbmisc", "tty", "input", NULL};
+	const gchar *subsystems[] = {"power_supply", "usbmisc", "input", NULL};
 
 	backend->priv->daemon = g_object_ref (daemon);
 	backend->priv->device_list = up_daemon_get_device_list (daemon);

commit dcb4600b3302d0f9e225a0db4c836f0a9725195a

enumerator-udev: also check for an idevice on "usb" subsystem
Commit 1550d50f ("linux: Remove "usb" subsystem match") broke detection of
some idevices, since it left just the "usbmisc" subsystem match while some
idevice / kernel combinations (at least an iPhone 11 on a 6.0 kernel) don't
present any such udev usbmisc devices.
However, they do present "usb" subsystem ones, so add this match back.

Leave idevice detection also for the "usbmisc" match since that's what the
original (known working) code before aforementioned commit did - it is
possible that it is required for some kernel / idevice combinations.

src/linux/up-enumerator-udev.c
@@ -120,6 +120,19 @@ device_new (UpEnumeratorUdev *self, GUdevDevice *native)
 		                       "native", native,
 		                       NULL);
 
+	} else if (g_strcmp0 (subsys, "usb") == 0) {
+#ifdef HAVE_IDEVICE
+		UpDevice *device;
+
+		device = g_initable_new (UP_TYPE_DEVICE_IDEVICE, NULL, NULL,
+		                         "daemon", daemon,
+		                         "native", native,
+		                         NULL);
+		if (device)
+			return device;
+#endif /* HAVE_IDEVICE */
+
+		return NULL;
 	} else if (g_strcmp0 (subsys, "usbmisc") == 0) {
 #ifdef HAVE_IDEVICE
 		UpDevice *device;
@@ -289,8 +302,8 @@ up_enumerator_udev_initable_init (UpEnumerator *enumerator)
 	guint i;
 	const gchar **subsystems;
 	/* List "input" first just to avoid some sibling hotplugging later */
-	const gchar *subsystems_no_wup[] = {"input", "power_supply", "usbmisc", NULL};
-	const gchar *subsystems_wup[] = {"input", "power_supply", "usbmisc", "tty", NULL};
+	const gchar *subsystems_no_wup[] = {"input", "power_supply", "usb", "usbmisc", NULL};
+	const gchar *subsystems_wup[] = {"input", "power_supply", "usb", "usbmisc", "tty", NULL};
 
 	config = up_config_new ();
 	if (up_config_get_boolean (config, "EnableWattsUpPro"))

src/linux/integration-test.py
@@ -2291,6 +2291,14 @@ class Tests(dbusmock.DBusTestCase):
                             'remove')
         self.testbed.uevent('/sys/devices/pci0000:00/0000:00:1a.0/usb1/1-1/1-1.1/1-1.1:1.2/0003:056A:0084.0021/input/input129',
                             'remove')
+        self.testbed.uevent('/sys/devices/pci0000:00/0000:00:1a.0/usb1/1-1/1-1.1/1-1.1:1.0',
+                            'remove')
+        self.testbed.uevent('/sys/devices/pci0000:00/0000:00:1a.0/usb1/1-1/1-1.1/1-1.1:1.1',
+                            'remove')
+        self.testbed.uevent('/sys/devices/pci0000:00/0000:00:1a.0/usb1/1-1/1-1.1/1-1.1:1.2',
+                            'remove')
+        self.testbed.uevent('/sys/devices/pci0000:00/0000:00:1a.0/usb1/1-1/1-1.1',
+                            'remove')
         self.daemon_log.check_line('No devices with parent /sys/devices/pci0000:00/0000:00:1a.0/usb1/1-1/1-1.1 left', timeout=2)
 
         # Ensure the object is really gone from the bus,