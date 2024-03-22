8cb9fb02319775bd9186c2f90f346acbf41b85f2
Kate Hsuan <hpa@redhat.com>
2024-03-22 20:01:04 +0800

    up-config: The warning message for the risky CriticalPowerAction
    The warning message will be shown when CriticalPowerAction is "Suspend".
    One iwarning message is for enabling the risky CriticalPowerAction and
    the other is to show the potential risk when
    AllowRiskyCriticalPowerAction is true.

--- src/up-config.c
+++ src/up-config.c
@@ -94,9 +94,11 @@ up_config_class_init (UpConfigClass *klass)
 static void
 up_config_init (UpConfig *config)
 {
-	gboolean ret;
+	gboolean allow_risky_critical_action = FALSE;
+	g_autofree gchar *critical_action = NULL;
 	GError *error = NULL;
 	gchar *filename;
+	gboolean ret;
 
 	config->priv = up_config_get_instance_private (config);
 	config->priv->keyfile = g_key_file_new ();
@@ -115,6 +117,25 @@ up_config_init (UpConfig *config)
 		g_warning ("failed to load config file '%s': %s",
 			   filename, error->message);
 		g_error_free (error);
+	}
+
+	/* Warn for any dangerous configurations */
+	critical_action = up_config_get_string (config, "CriticalPowerAction");
+	allow_risky_critical_action = up_config_get_boolean (config, "AllowRiskyCriticalPowerAction");
+
+	if (!g_strcmp0 (critical_action, "Suspend")) {
+		if (allow_risky_critical_action) {
+			g_warning ("The \"Suspend\" CriticalPowerAction setting is considered risky:"
+				   " abrupt power loss due to battery exhaustion may lead to data"
+				   " corruption. Use AllowRiskyCriticalPowerAction=false to disable"
+				   " support for risky settings.");
+		} else {
+			g_warning ("The \"Suspend\" CriticalPowerAction setting is considered risky:"
+				   " abrupt power loss due to battery exhaustion may lead to data"
+				   " corruption. The system will perform \"HybridSleep\" instead."
+				   " Use AllowRiskyCriticalPowerAction=true to enable support for"
+				   " risky settings.");
+		}
 	}
 
 	g_free (filename);
